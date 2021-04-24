0077c196ec7dfa1bcd589e5a0e4c21f9f52aa935
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-04-24 20:46:53 +0600

    Header file with important preprocessor, linker and global variable created

---
+++ src/constants.h
@@ -0,0 +1,16 @@
+//Controlling redefinition
+#ifndef __constants__
+#define __constants__
+
+#include <iostream>
+
+//Preprocessor to work with SDL
+#include <SDL2/SDL.h>
+
+//Preprocessor to work with SDL Image Funtionalities
+#include <SDL2/SDL_image.h>
+
+#define FALSE 0
+#define TRUE 1
+
+#endif

1b1fb8d21223e34965ca763509cf8e4cb849eda7
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-05-04 09:34:46 +0600

    Public Instance of Game CLass Created Making it Singleton. A vector to Store Game Object Created.

--- src/Game.h
+++ src/Game.h
@@ -4,16 +4,16 @@
 
 //All The header file included and other global variable declared
 #include "constants.h"
+#include"Gameobject.h"
 
 //Game class initialization
 class Game
 {
 public:
-    Game();  // Constructor
     ~Game(); // Destructor
 
     //SDL game initialization
-    void init(const char *title, int xpos, int ypos, int width, int height, int flags);
+    bool init(const char *title, int xpos, int ypos, int width, int height, int flags);
     //Hadling all events namely inputs
     void handleEvents();
     //Updating according to input
@@ -24,13 +24,36 @@ public:
     void clean();
     //function to check if the game is over or not
     bool running();
+    // create the public instance function
+    static Game *Instance()
+    {
+        if (s_pInstance == 0)
+        {
+            s_pInstance = new Game();
+            return s_pInstance;
+        }
+        return s_pInstance;
+    }
+    //will return our SDL_Renderer object
+    SDL_Renderer *getRenderer() const { return m_pRenderer; }
 
 private:
+    // make the constructor private
+    Game();
+    // create the s_pInstance member variable
+    static Game *s_pInstance;
     //Creating a SDL_Window Pointer to display a window
     SDL_Window *m_pWindow;
     //Creating a SDL_Renderer Pointer to render everything
     SDL_Renderer *m_pRenderer;
+    //For Time Calculaton
+    int last_frame_time = 0;
+    //A vector to store Game object
+    std::vector<GameObject *> m_gameObjects;
     //A bool variable soring if the game is running or not
     bool m_bRunning = FALSE;
 };
+
+// create the typedef
+typedef Game TheGame;
 #endif

e73794ad43dfe2159ffa4ecdba71bd5f0e707463
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-05-04 09:30:21 +0600

    Main Program Updated, instance of game class used, making it more readable and easy to understand.

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,27 @@ Game *my_first_game = 0;
 //Main Function
 int main(int argc, char *argv[])
 {
-    //New Game Constructor
-    my_first_game = new Game();
 
     //initializing game
-    my_first_game->init("WOI-1971", 100, 100, 1280, 720, 0);
-    printf("\nWW      WW  OOOOO  IIIII         1          7777777  1\nWW      WW OO   OO  III         111  99999      777 111\nWW   W  WW OO   OO  III  _____   11 99   99    777   11\n WW WWW WW OO   OO  III          11  999999   777    11\n  WW   WW   OOOO0  IIIII        111     99   777    111\n                                      999\n              \n");
-    printf("Started Successfully! Enjoy!\n");
-    //Checking if the game is running
-    while (my_first_game->running())
+    if (TheGame::Instance()->init("WOI", 100, 100, 1280, 720, FALSE))
     {
-        //Render my objects onto screen
-        my_first_game->render();
+        //game init successful message
+        printf("\nWW      WW  OOOOO  IIIII         1          7777777  1\nWW      WW OO   OO  III         111  99999      777 111\nWW   W  WW OO   OO  III  _____   11 99   99    777   11\n WW WWW WW OO   OO  III          11  999999   777    11\n  WW   WW   OOOO0  IIIII        111     99   777    111\n                                      999\n              \n");
+        printf("Started Successfully! Enjoy!\n");
 
-        //Handles all the Events
-        my_first_game->handleEvents();
-
-        //Updating after handling event
-        my_first_game->update();
+        //Checking if the game is running
+        while (TheGame::Instance()->running())
+        {
+            //Render my objects onto screen
+            TheGame::Instance()->handleEvents();
+            //Handles all the Events
+            TheGame::Instance()->update();
+            //Display content or Update the screen after handling event
+            TheGame::Instance()->render();
+        }
     }
-
-    //Cleaning my memory by deleting all the memory related stuff
-    my_first_game->clean();
-
+     //Cleaning my memory by deleting all the memory related stuff
+    TheGame::Instance()->clean();
     //Successfully done and retrun 0
     return 0;
 }

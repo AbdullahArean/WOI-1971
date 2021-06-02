6a57a557f3a9b4c7aa703df91a65a8c086f1e42f
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-06-02 17:03:22 +0600

    Small Change to make code more readable

--- src/constants.h
+++ src/constants.h
@@ -4,41 +4,47 @@
 //Preprocessors
 #include <stdio.h>
 
+//SDL Related Libraries
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_image.h>
 
-
+//Important files to linked
 #include"texturemanager.h"
 #include "clean.h"
-//#include"game.h"
-
-bool game_running=1;
-
-SDL_Window *window;
-SDL_Renderer *renderer;
-// speed in pixels/second
 
 //Defining Macro
 #define FALSE 0
 #define TRUE 1
+
 //Window Height Width
-#define WINDOW_WIDTH 1080 
-#define WINDOW_HEIGHT 720
+// #define WINDOW_WIDTH 1080 
+// #define WINDOW_HEIGHT 720
 
 //frame rate per Second
 #define FPS 60
 #define FRAME_TARGET_TIME (1000 / FPS)
-SDL_Texture *fpage = NULL;
-TextureManager menu;
+
+
+//ALL The Global Varaiables Declared Here
+//variable to keep game state
+bool game_running=1;
+
+//SDL Window & Renderer
+SDL_Window *window;
+SDL_Renderer *renderer;
+
+//Texture Manager
 TextureManager TM;
+
+//SDL Textures
+SDL_Texture *fpage = NULL;
 SDL_Texture *bg = NULL;
 SDL_Texture *con = NULL;
 SDL_Texture *newg = NULL;
 SDL_Texture *his = NULL;
 SDL_Texture *settings = NULL;
 SDL_Texture *story = NULL;
-
 SDL_Texture *conp = NULL;
 SDL_Texture *newgp = NULL;
 SDL_Texture *hisp = NULL;
@@ -46,6 +52,6 @@ SDL_Texture *settingsp = NULL;
 SDL_Texture *storyp = NULL;
 
 
-#define SPEED (300)
+//#define SPEED (300)
 
 #endif
--- src/mainmenu.h
+++ src/mainmenu.h
@@ -1,41 +1,28 @@
 #include "constants.h"
 
 void mainmenuinit()
-{
-    // SDL_Texture *bg = menu.ReturnTexture("assets/buttonbackground.png", renderer);
-    // SDL_Texture *con = menu.ReturnTexture("assets/continue.png", renderer);
-    // SDL_Texture *newg = menu.ReturnTexture("assets/newgame.png", renderer);
-    // SDL_Texture *his = menu.ReturnTexture("assets/highscore.png", renderer);
-    // SDL_Texture *settings = menu.ReturnTexture("assets/settings.png", renderer);
-    // SDL_Texture *story = menu.ReturnTexture("assets/story.png", renderer);
-
-    // SDL_Texture *conp = menu.ReturnTexture("assets/continuepressed.png", renderer);
-    // SDL_Texture *newgp = menu.ReturnTexture("assets/newgamepressed.png", renderer);
-    // SDL_Texture *hisp = menu.ReturnTexture("assets/highscorep.png", renderer);
-    // SDL_Texture *settingsp = menu.ReturnTexture("assets/settingsp.png", renderer);
-    // SDL_Texture *storyp = menu.ReturnTexture("assets/storyp.png", renderer);
-    
-    bg = menu.ReturnTexture("assets/buttonbackground.png", renderer);
-    con = menu.ReturnTexture("assets/continue.png", renderer);
-    newg = menu.ReturnTexture("assets/newgame.png", renderer);
-    his = menu.ReturnTexture("assets/highscore.png", renderer);
-    settings = menu.ReturnTexture("assets/settings.png", renderer);
-    story = menu.ReturnTexture("assets/story.png", renderer);
-    conp = menu.ReturnTexture("assets/continuepressed.png", renderer);
-    newgp = menu.ReturnTexture("assets/newgamepressed.png", renderer);
-    hisp = menu.ReturnTexture("assets/highscorep.png", renderer);
-    settingsp = menu.ReturnTexture("assets/settingsp.png", renderer);
-    storyp = menu.ReturnTexture("assets/storyp.png", renderer);
+{    
+    bg = TM.ReturnTexture("assets/buttonbackground.png", renderer);
+    con = TM.ReturnTexture("assets/continue.png", renderer);
+    newg = TM.ReturnTexture("assets/newgame.png", renderer);
+    his = TM.ReturnTexture("assets/highscore.png", renderer);
+    settings = TM.ReturnTexture("assets/settings.png", renderer);
+    story = TM.ReturnTexture("assets/story.png", renderer);
+    conp = TM.ReturnTexture("assets/continuepressed.png", renderer);
+    newgp = TM.ReturnTexture("assets/newgamepressed.png", renderer);
+    hisp = TM.ReturnTexture("assets/highscorep.png", renderer);
+    settingsp = TM.ReturnTexture("assets/settingsp.png", renderer);
+    storyp = TM.ReturnTexture("assets/storyp.png", renderer);
 }
 void mainmenurender()
 {
 
-    menu.drawsame(0, 0, bg, renderer);
-    menu.drawsame(405, 95, con, renderer);
-    menu.drawsame(405, 217, newg, renderer);
-    menu.drawsame(405, 339, his, renderer);
-    menu.drawsame(405, 461, settings, renderer);
-    menu.drawsame(405, 583, story, renderer);
+    TM.drawsame(0, 0, bg, renderer);
+    TM.drawsame(405, 95, con, renderer);
+    TM.drawsame(405, 217, newg, renderer);
+    TM.drawsame(405, 339, his, renderer);
+    TM.drawsame(405, 461, settings, renderer);
+    TM.drawsame(405, 583, story, renderer);
 }
 void mainmenuupdate()
 {
@@ -48,27 +35,27 @@ void mainmenuupdate()
         if (mousex >= 405 && mousex <= 405 + 278 && mousey >= 95 && mousey <= 95 + 91)
         {
             // printf("%d %d\n", mousex, mousey);
-            menu.drawsame(405, 95, conp, renderer);
+            TM.drawsame(405, 95, conp, renderer);
         }
         if (mousex >= 405 && mousex <= 405 + 278 && mousey >= 217 && mousey <= 217 + 91)
         {
             // printf("%d %d\n", mousex, mousey);
-            menu.drawsame(405, 217, newgp, renderer);
+            TM.drawsame(405, 217, newgp, renderer);
         }
         if (mousex >= 405 && mousex <= 405 + 278 && mousey >= 339 && mousey <= 339 + 91)
         {
             // printf("%d %d\n", mousex, mousey);
-            menu.drawsame(405, 339, hisp, renderer);
+            TM.drawsame(405, 339, hisp, renderer);
         }
         if (mousex >= 405 && mousex <= 405 + 278 && mousey >= 461 && mousey <= 461 + 91)
         {
             // printf("%d %d\n", mousex, mousey);
-            menu.drawsame(405, 461, settingsp, renderer);
+            TM.drawsame(405, 461, settingsp, renderer);
         }
         if (mousex >= 405 && mousex <= 405 + 278 && mousey >= 583 && mousey <= 583 + 91)
         {
             // printf("%d %d\n", mousex, mousey);
-            menu.drawsame(405, 583, storyp, renderer);
+            TM.drawsame(405, 583, storyp, renderer);
         }
     }
 }

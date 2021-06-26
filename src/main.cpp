e2d14f04c38ad58869d58342e0d0c44f574b55b8
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-06-26 18:39:47 +0600

    main.cpp properly commented

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,31 @@
+//User Defined Header File
 #include "constants.h"
 #include "game.h"
-#include "player.h"
 
 
-
+//Main Function
 int main(int argc, char *args[])
 {
-    
+    //Making Game Structure
     Game myfirstgame;
+
+    //Initializing Game  
     myfirstgame.init();
+
+    //Making a loop when game is running
     while(myfirstgame.running())
     {
+        //Handling Events like input
         myfirstgame.handleEvents();
+        //Updating the screen accordingly
         myfirstgame.update();
+        //Displaying 
         myfirstgame.render();
     }
+    //Exit from the game 
     myfirstgame.destroy_window();
+
+    //Successfully Executed 
     return 0;
 }
 

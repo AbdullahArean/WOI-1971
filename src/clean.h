94997bfa71187b8de7976eb9bf872c583cec1949
AbdullahArean <2019-917-795@student.cse.du.ac.bd>
2021-06-26 19:02:27 +0600

    clean.h organized and readable

--- src/clean.h
+++ src/clean.h
@@ -2,22 +2,19 @@
 #define CLEAN_H
 
 //Preprocessors
-#include"constants.h"
+#include "constants.h"
 #include "sound.h"
 
-
-
-
 //Destroying the window and the renderer
-void Clean(SDL_Window* win, SDL_Renderer* render)
+void Clean(SDL_Window *win, SDL_Renderer *render)
 {
     soundclose();
-    SDL_DestroyRenderer(render);//Destroying Renderer
-    SDL_DestroyWindow(win);//Destroying Window
-    TTF_Quit();//Quiting SDL_ttf
-    Mix_Quit();//Quiting SDL_mixer
-    IMG_Quit();//Quiting SDL Image
-    SDL_Quit();//Quiting SDL
+    SDL_DestroyRenderer(render); //Destroying Renderer
+    SDL_DestroyWindow(win);      //Destroying Window
+    TTF_Quit();                  //Quiting SDL_ttf
+    Mix_Quit();                  //Quiting SDL_mixer
+    IMG_Quit();                  //Quiting SDL Image
+    SDL_Quit();                  //Quiting SDL
 }
 
-#endif 
+#endif
